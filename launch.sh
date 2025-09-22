b1fa399c0c8f248b466c80dbab6c548738d4d98f
PiotrDuz <39244523+PiotrDuz@users.noreply.github.com>
2025-09-22 23:16:39 +0200

    logs

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/dzawor/new_venv/bin/python /home/dzawor/valve/main.py 2>&1 | rotatelogs logs.log 100M
+/home/dzawor/new_venv/bin/python /home/dzawor/valve/main.py 2>&1 | rotatelogs /home/dzawor/logs.log 100M
