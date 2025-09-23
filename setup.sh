ee3cd4356bc33470baa45f60f3b463af2cc1b73d
PiotrDuz <39244523+PiotrDuz@users.noreply.github.com>
2025-09-23 21:13:21 +0200

    setup

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# REMEMBER TO ENABLE i2c manually !
+# username is: dzawor
+sudo apt-get update
+sudo apt-get -y install python3
+git clone https://github.com/PiotrDuz/valve.git
+python3 -m venv new_venv
+new_venv/bin/pip install -r valve/requirements.txt
+echo 'setup files'
+chmod +x valve/restartValve.sh
+sudo mv valve/restartValve.sh /usr/sbin
+chmod +x valve/setWifi.sh
+sudo mv valve/setWifi.sh /usr/sbin
+chmod +x valve/startAP.sh
+sudo mv valve/startAP.sh /usr/sbin
+sudo echo 'dzawor ALL=(ALL) NOPASSWD: /usr/sbin/setWifi.sh' >> /etc/sudoers
+sudo echo 'dzawor ALL=(ALL) NOPASSWD: /usr/sbin/restartValve.sh' >> /etc/sudoers
+sudo echo 'dzawor ALL=(ALL) NOPASSWD: /usr/sbin/startAP.sh' >> /etc/sudoers
+sudo apt-get install apache2-utils
+chmod +x valve/launch.sh
+echo "
+[Unit]
+Description=dzawor service
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=dzawor
+ExecStart=${PWD}/valve/launch.sh
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/dzawor.service
+#systemctl enable dzawor
